82861483993d546ec80a69630a5818bc84220e59
Bruno Perel <brunoperel@gmail.com>
2017-09-09 18:12:40 +0200

    Update following Youtube's DOM changes

--- youtube-caption-indicator.js
+++ youtube-caption-indicator.js
@@ -2,11 +2,12 @@
 // @name         Youtube caption indicator
 // @namespace    youtube-caption-indicator
 // @require      https://code.jquery.com/jquery-3.1.1.min.js
-// @version      0.1
+// @version      0.2
 // @description  Add language-specific caption information in YouTube video blocks
 // @author       Bruno Perel
 // @match        https://www.youtube.com/*
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
 (function () {
@@ -29,7 +30,7 @@
     function getCCInfo(videoId, callback) {
         var ccInfo = null;
         if (typeof GM_getValue === "function") {
-            ccInfo = GM_getValue('youtube-caption-indicator-'+videoId);
+            ccInfo = GM_getValue('youtube-caption-indicator-' + videoId);
             if (ccInfo) {
                 ccInfo = JSON.parse(ccInfo);
                 if (current_timestamp - ccInfo.timestamp < cache_ttl) {
@@ -41,47 +42,66 @@
                     console.info(videoId + ': Cache expired, retrieving CC info again');
                 }
             }
-        }
-        $.getJSON('https://www.googleapis.com/youtube/v3/captions?part=snippet&videoId=' + videoId + '&key=' + api_key)
-            .done(function (data) {
-                callback(data.items);
+            $.getJSON('https://www.googleapis.com/youtube/v3/captions?part=snippet&videoId=' + videoId + '&key=' + api_key)
+                .done(function (data) {
+                    callback(data.items);
 
-                if (typeof GM_setValue === 'function') {
-                    GM_setValue('youtube-caption-indicator-'+videoId, JSON.stringify({timestamp: current_timestamp, items: data.items }));
-                    console.info(videoId + ': Stored in cache for ' + cache_ttl + 'seconds');
-                }
-            })
-            .fail(function (jqxhr, textStatus, error) {
-                var err = textStatus + ", " + error;
-                console.error(videoId + ': Request Failed: ' + err);
-            });
+                    if (typeof GM_setValue === 'function') {
+                        GM_setValue('youtube-caption-indicator-' + videoId, JSON.stringify({
+                            timestamp: current_timestamp,
+                            items: data.items
+                        }));
+                        console.info(videoId + ': Stored in cache for ' + cache_ttl + 'seconds');
+                    }
+                })
+                .fail(function (jqxhr, textStatus, error) {
+                    var err = textStatus + ", " + error;
+                    console.error(videoId + ': Request Failed: ' + err);
+                });
+        }
+        else {
+            console.error('GM_getValue is undefined !');
+        }
     }
 
-    function addCCInfoToVideoBlockElement(videoId, blockElement) {
-        getCCInfo(videoId, function(ccInfo) {
+    function getCCInfoElement(language) {
+        var text = language ? ('CC ' + language) : '(no CC)';
+        return $('<span>')
+            .addClass('badge badge-style-type-collection style-scope ytd-badge-supported-renderer')
+            .css({display: 'inline-block', paddingRight: '10px'})
+            .append(
+                $('<span>')
+                    .addClass('style-scope ytd-badge-supported-renderer')
+                    .text(text)
+            );
+    }
+
+    function addCCInfoToVideoBlockElement(videoUrl, blockElement) {
+        var videoId = videoUrl.replace(/^.+\?v=(.+)$/, '$1');
+        if (videoId === 'qiuDE38TEpU') {
+            debugger;
+        }
+        getCCInfo(videoId, function (ccInfo) {
             var ccNewElements = $.map(ccInfo, function (ccItem) {
                 if (!ccItem.snippet) {
                     console.error(videoId + ': Malformed CC item: no snippet key, ignoring');
                 }
                 var isAuto = ccItem.snippet.trackKind === 'ASR';
-                var language = ccItem.snippet.language;
-                var text = 'CC ' + (isAuto ? 'auto' : language);
+                var language = isAuto ? 'auto' : ccItem.snippet.language;
 
-                return $('<li>')
-                    .addClass('yt-badge-item')
-                    .append(
-                        $('<span>')
-                            .addClass('yt-badge')
-                            .text(text)
-                    );
+                return getCCInfoElement(language);
             });
+
+            if (ccInfo.length === 0) {
+                ccNewElements = [getCCInfoElement(null)];
+            }
 
             blockElement.html(ccNewElements);
         });
     }
 
-    function getBadgeContainer(videoBlockElement) {
-        var badgesContainer = videoBlockElement.find('div.yt-lockup-badges');
+    function getBadgeContainer(metaContainer) {
+        var badgesContainer = metaContainer.find('div.yt-lockup-badges');
         return badgesContainer.length ? badgesContainer : null;
     }
 
@@ -91,51 +111,45 @@
 
     function createIndicatorFromSearchResults() {
         var videoBlockElement = $(this);
-        var videoId = videoBlockElement.attr('data-context-item-id');
         var ccDetailsContainer;
-        var badgesContainer = getBadgeContainer(videoBlockElement);
-        var descriptionContainer = videoBlockElement.find('.yt-lockup-description');
-        var metaContainer = videoBlockElement.find('.yt-lockup-meta');
+        var metaContainer = videoBlockElement.find('#meta,#metadata').eq(0);
+        var badgesContainer = getBadgeContainer(metaContainer);
+        var videoUrl = videoBlockElement.find('a[href^="/watch"]').eq(0).attr('href');
         if (!!badgesContainer) {
             ccDetailsContainer = badgesContainer.find('ul.yt-badge-list');
             var ccElements = ccDetailsContainer.find('li.yt-badge-item');
             if (ccElements.length === 0) {
                 console.log(videoId + ': No caption for this video, ignoring');
-                return;
             }
             else if (hasProcessedBadges(ccDetailsContainer)) {
                 console.log(videoId + ': Caption list already processed, ignoring');
-                return;
             }
         }
         else {
-            addEmptyBadgeSectionAfter(descriptionContainer.length ? descriptionContainer : metaContainer);
-            ccDetailsContainer = videoBlockElement.find('ul.yt-badge-list');
-        }
-        addCCInfoToVideoBlockElement(videoId, ccDetailsContainer);
-    }
-
-    function createIndicatorFromRelatedVideos() {
-        var videoBlockElement = $(this);
-        var videoId = videoBlockElement.find('a.content-link').attr('href').replace(/^.+\?v=(.+)$/, '$1');
-
-        var badgesContainer = getBadgeContainer(videoBlockElement);
-        if (!!badgesContainer) {
-            console.log(videoId + ': Caption list already processed, ignoring');
-        }
-        else {
-            addEmptyBadgeSectionAfter(videoBlockElement.find('.stat.view-count'));
-            addCCInfoToVideoBlockElement(videoId, videoBlockElement.find('ul.yt-badge-list'));
+            if (videoUrl) {
+                addEmptyBadgeSectionAfter(metaContainer);
+                ccDetailsContainer = videoBlockElement.find('ul.yt-badge-list');
+                addCCInfoToVideoBlockElement(videoUrl, ccDetailsContainer);
+            }
+            else {
+                console.log('Can\'t find the video\'s URL, ignoring');
+            }
         }
     }
 
-    $('body')
-        .on('DOMNodeInserted', '[data-context-item-id]', createIndicatorFromSearchResults)
-        .on('DOMNodeInserted', '.related-list-item-compact-video, .autoplay-bar .video-list-item', createIndicatorFromRelatedVideos);
+    var videoBlockSelector = 'ytd-grid-video-renderer,ytd-compact-video-renderer';
 
-    $('[data-context-item-id]').each(createIndicatorFromSearchResults);
+    new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            $(mutation.addedNodes).filter(videoBlockSelector).each(createIndicatorFromSearchResults);
+        });
+    })
+        .observe($('body').get(0), {
+            childList: true,
+            subtree: true
+        });
 
-    $('.related-list-item-compact-video, .autoplay-bar .video-list-item').each(createIndicatorFromRelatedVideos);
+    $(videoBlockSelector).each(createIndicatorFromSearchResults);
 
 
 })();
