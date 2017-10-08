a6870367064c46e2323a0bb9e855c6b507e3bc23
Bruno Perel <brunoperel@gmail.com>
2017-10-08 14:54:57 +0200

    Simplify packing script

--- pack.sh
+++ pack.sh
@@ -2,12 +2,10 @@
 
 build_dir=./build/
 
-rm -f youtube-caption-indicator.zip && \
-cp bower_components/jquery/dist/jquery.min.js youtube-caption-indicator.js manifest.json ${build_dir} && \
+rm -rf youtube-caption-indicator.zip ${build_dir} && mkdir ${build_dir} && \
+\
+cp bower_components/jquery/dist/jquery.min.js youtube-caption-indicator.js manifest.json icon.png ${build_dir} && \
 sed -i "s/__API_KEY__/`cat api_key`/g" ${build_dir}/youtube-caption-indicator.js && \
 sed -i "s/\".*jquery.min.js\"/\"jquery.min.js\"/g" ${build_dir}/manifest.json && \
 \
-zip -j youtube-caption-indicator.zip \
-    ${build_dir}/* \
-    README.md \
-    icon.png
+zip -j youtube-caption-indicator.zip ${build_dir}/*
